94faa18b6d353721f28d4df8cc229f86d38669dd
elementtest <elementtest7@gmail.com>
2022-08-01 09:45:42 +0800

    add test file(test git script)

--- git-script.sh
+++ git-script.sh
@@ -3,5 +3,8 @@
 git add *
 read -p "git commit description: " desc
 git commit -m "$desc"
+#use git push origin branch-name instead of git push origin maijn 
+#used to test new features on new branch and then merge to main trunk 
+#main trunk is called main
 git push origin rps-ui
 
